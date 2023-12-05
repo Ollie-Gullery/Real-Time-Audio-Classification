d4afbe844df97c896d18cc2e76f0e38847fdad16
Ollie G <115392875+Ollie-Gullery@users.noreply.github.com>
2023-12-04 16:21:50 -0800

    Completed Processing Data
    Converted data to MFCCs and storing it in json file in processed data folder

--- src/prepare_dataset.py
+++ src/prepare_dataset.py
@@ -1,15 +1,16 @@
 import librosa
 import os
 import json
+import numpy as np
 
-DATASET_PATH = "dataset"
-JSON_PATH = "data.json"
-SAMPLES_TO_CONSIDER = 22050 # 1 sec worth of loading sound with librosa
+DATASET_PATH = "data/raw/dataset"
+JSON_PATH = "data/processed/data.json"
+SAMPLES_TO_CONSIDER = 22050 * 30 # 1 sec worth of loading audio with librosa is 22050 samples/sec, multiply by 30 for 30 seconds, standardizes length of the audio files
 
 
-def prepare_dataset(dataset_path, json_path, n_mfcc=13, n_fft=2048, hop_length=512):
+def preprocess_dataset(dataset_path, json_path, n_mfcc=13, n_fft=2048, hop_length=512):
     """_summary_
-    Extracts MFCCs from music dataset and saves them into a json file
+    Extracts MFCCs from music dataset and saves them into a json file located in our processed data folder 
   
     Args:
         dataset_path (_type_): _description_
@@ -18,45 +19,52 @@ def prepare_dataset(dataset_path, json_path, n_mfcc=13, n_fft=2048, hop_length=5
         n_fft (int, optional): _description_. Defaults to 2048.
         hop_length (int, optional): _description_. Defaults to 512.
     """
-
     # data dictoinary
     
     data = {
-        "mappings": ["Music", "Speech", ...],
-        "labels": [0,0,1,1,...],
+        "mappings": [],
+        "labels": [],
         "MFCCs": [],
         "files": []
     }
 
     # loop through all sub-dirs
-    for i in os.walk(dataset_path):
-        print(i)
+    for i, (dirpath, dirnames, filenames) in enumerate(os.walk(dataset_path)):
+        
 
-    # ensure we're at sub-folder level
-
-    # save label (i.e., sub-folder name) in the mapping
-
-    # process all audio files in sub-dir and store MFCCs
-
-    # load audio file and slice it to ensure length consistency among different files
-
-    # drop audio files with less than pre-decided number of samples
-
-    # ensure consistency of the length of the signal
-
-    # extract MFCCs
-
-    # store data for analysed track
-
+        # ensure we're at sub-folder level
+        if dirpath != dataset_path:
+            
+            # save label (i.e., sub-folder name) in the mapping
+            label = dirpath.split("/")[-1]
+            data["mappings"].append(label)
+            print("\nProcessing: '{}'".format(label))
+            
+            # process all audio files in sub-dir and store MFCCs
+            for file in filenames:
+                temp = []
+                
+                file_path = os.path.join(dirpath, file)
+                
+                signal, sample_rate = librosa.load(file_path) 
+                
+                # dropping audio files with less than pre-decided number of samples 
+                if len(signal) >= SAMPLES_TO_CONSIDER: 
+                    
+                    signal = signal[:SAMPLES_TO_CONSIDER] # ensure consistency of the length of the signal
+                    
+                    # Extract MFCCs
+                    MFCCs = librosa.feature.mfcc(y=signal, sr=sample_rate, n_mfcc=n_mfcc, n_fft=n_fft,hop_length=hop_length)
+                    
+                    # Store data for analysed track
+                    data["MFCCs"].append(MFCCs.T.tolist())
+          
+            # print(data["MFCCs"]) # To check outputs 
+    
     # save data in json file
-# for i,k in os.walk("/dataset"):
-#         print("test")
-#         print(f'i here: {i}')
-#         print(f'k here: {k}')
+    with open(json_path, "w") as fp:
+        json.dump(data, fp, indent=4)
 
-print("test")
-
-for i in os.walk("/dataset"):
-    print(i)
-
-print("hello") 
+# Ensures method is only executed when script is run directly, not when it is imported as a module in another script
+if __name__ == "__main__": 
+    preprocess_dataset(DATASET_PATH, JSON_PATH)
